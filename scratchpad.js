84ea20e32510df26f788db20b671af9a8c0f7ccf
@ForSerious <forxxserious@gmail.com>
2017-03-09 15:50:51 -0700

    Coppied fib helper and changed it up into pell helper.

--- scratchpad.js
+++ scratchpad.js
@@ -43,6 +43,43 @@ function fibHelper(n)
 	}
 	return { 'value': value, 'html': div };
 }
+
+function pellHelper(n) {
+    var value;
+    var div = document.createElement('div');
+    div.setAttribute("class", "pell");
+
+    // leaf nodes aka. base case
+    if (n < 2) {
+        if (n === 0) {
+            value = 0;
+        }
+        else if (n === 1) {
+            value = 1;
+        }
+        var p = document.createElement('p');
+        p.textContent = 'Fib(' + n + ') = ' + value;
+        div.appendChild(p)
+    }
+    else {
+        var left = pellHelper(n - 1)*2;
+        var clas = left.html.getAttribute("class");
+        left.html.setAttribute("class", clas + " pell-left");
+
+        var right = pellHelper(n - 2);
+        clas = right.html.getAttribute("class");
+        right.html.setAttribute("class", clas + " pell-right");
+
+        value = left.value + right.value;
+        var p = document.createElement('p');
+        p.textContent = 'pell(' + n + ') = ' + value;
+        div.appendChild(p);
+
+        div.appendChild(left.html);
+        div.appendChild(right.html);
+    }
+    return { 'value': value, 'html': div };
+}
 var fib = function (n, node) {
 	var tree = fibHelper(n)
 		node.appendChild(tree.html);
